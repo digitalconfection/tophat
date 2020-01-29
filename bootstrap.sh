0aebba27c50f4ed8b1e5a6491dbbf5b88dfa469e
Misha Vinokur <misha@Mishas-MacBook-Pro.local>
2020-01-29 10:09:03 -0500

    minor items

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ sudo apt-get install -y php7.2 -y
 
 # MySql
 sudo apt install mysql-server -y
-sudo mysql_secure_installation
+# sudo mysql_secure_installation
 
 # Composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
@@ -26,7 +26,5 @@ sudo service apache2 restart
 # Clean Up Box
 sudo apt-get clean -y
 sudo dd if=/dev/zero of=/EMPTY bs=1M
-sudo rm -f /EMPTY -y
+sudo rm -f /EMPTY
 sudo cat /dev/null > ~/.bash_history && history -c && exit
-
-
