5028f1b4c912f5ec4a6b5f8b085dbdbb30340777
Misha Vinokur <misha@Mishas-MacBook-Pro.local>
2020-01-30 09:30:32 -0500

    added missing library

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade -y
 sudo apt-get install apache2 -y
 
 # PHP (7.2)
-sudo apt-get install php7.2 php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-zip php7.2-fpm php7.2-curl php7.2-xml php7.2-gd php7.2-zip -y
+sudo apt-get install php7.2 php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-zip php7.2-fpm php7.2-curl php7.2-xml php7.2-gd php7.2-zip libapache2-mod-php7.2 -y
 sudo a2enmod php7.2
 sudo a2enmod rewrite
 sudo systemctl restart apache2
