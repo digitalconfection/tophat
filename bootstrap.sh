766c7c559aa88db6882b3b2c3903f8fe52f64077
Misha Vinokur <misha@Mishas-MacBook-Pro.local>
2020-01-30 01:30:50 -0500

    updated php 7.2 build

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,10 @@ sudo apt-get upgrade -y
 sudo apt-get install apache2 -y
 
 # PHP
-sudo apt install php7.2-cli -y
-sudo apt-get install -y php7.2 -y
+sudo apt-get install php7.2 php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-zip php7.2-fpm php7.2-curl php7.2-xml php7.2-gd php7.2-zip -y
+sudo a2enmod php7.2
+sudo a2enmod rewrite
+sudo systemctl restart apache2
 
 # MySql
 sudo apt install mysql-server -y
@@ -22,6 +24,7 @@ sudo apt-get install npm -y
 
 # Restart Services
 sudo service apache2 restart
+sudo service mysql restart
 
 # Clean Up Box
 sudo apt-get clean -y
